28ef6fafda09ff6984a89bf9b1a0138b54508926
mckinseyleckenby <leckenbym11@gmail.com>
2022-02-10 09:11:24 -0600

    first commit

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-const btn = document.querySelector(".btn");
-const result = document.querySelector(".result");
+const word = document.querySelector(".input-text")
+const btn = document.querySelector(".btn")
+const result = document.querySelector(".result")
 
-btn.addEventListener("click", palindrome);
-console.log("hi")
+btn.addEventListener("click", countVowel)
 
-// madam
-function palindrome() {
-    const word = document.querySelector(".input-text").value;
-    let len = word.length;
-
-    let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
-    let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
-
-    // let flip = end.split('').reverse().join("");
-    let flip = [...end].reverse().join('');
-    if (start == flip) {
-        result.innerHTML = `${word.toUpperCase()} is a palindrome`
-    }else {
-        result.innerHTML = `${word.toUpperCase()} is not a palindrom`;
+function countVowel() {
+    let vowelCount = 0;
+    let wordValue = word.value.toLowerCase();
+   // alert(wordValue)
+    for (let i = 0; i < wordValue.length; i++) {
+        let letter = wordValue[i]
+        if (letter.match("([a, e, i, o, u])")) {
+            vowelCount ++;
+        }
     }
-
+    result.innerHTML = `${word.value.toUpperCase()} has ${vowelCount} vowels`
 
 }
+
+
+
+
